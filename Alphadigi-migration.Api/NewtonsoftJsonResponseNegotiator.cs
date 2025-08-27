cb1c3e812f1d74a275cc22a9cda6854997e679b0
Daniel Carvalho <danielscg2012@gmail.com>
2025-08-27 08:41:04 -0300

    Add Alphadigi-migration.Api as normal folder

---
+++ Alphadigi-migration.Api/NewtonsoftJsonResponseNegotiator.cs
@@ -0,0 +1,37 @@
+﻿namespace Carter.ResponseNegotiators.SystemTextJson;
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+/// <summary>
+/// A System.Text.Json implementation of <see cref="IResponseNegotiator"/>
+/// </summary>
+public class SystemTextJsonResponseNegotiator : IResponseNegotiator
+{
+    private readonly JsonSerializerOptions jsonOptions;
+
+    public SystemTextJsonResponseNegotiator()
+    {
+        this.jsonOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+    }
+
+    public bool CanHandle(MediaTypeHeaderValue accept)
+    {
+        return accept.MediaType.ToString().IndexOf("json", StringComparison.OrdinalIgnoreCase) >= 0;
+    }
+
+    public Task Handle<T>(HttpRequest req, HttpResponse res, T model, CancellationToken cancellationToken)
+    {
+        res.ContentType = "application/json; charset=utf-8";
+        return res.WriteAsync(JsonSerializer.Serialize(model, this.jsonOptions), cancellationToken);
+    }
+}
---
+++ Alphadigi-migration.Api/Program.cs
@@ -0,0 +1,111 @@
+using Alphadigi_migration;
+using Alphadigi_migration.Application.Services;
+using Alphadigi_migration.Domain.Interfaces;
+using Alphadigi_migration.Extensions;
+using Alphadigi_migration.Extensions.Options;
+using Alphadigi_migration.Infrastructure.Data;
+using Alphadigi_migration.Services;
+using Carter;
+using Carter.ResponseNegotiators.SystemTextJson;
+using IniParser;
+using IniParser.Model;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+builder.Services.Configure<PlateComparisonSettings>(
+    builder.Configuration.GetSection("PlateComparisonSettings")
+);
+
+builder.Services.AddCarter(configurator: c =>
+    c.WithResponseNegotiator<SystemTextJsonResponseNegotiator>()
+);
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.log",
+                  rollingInterval: RollingInterval.Day,
+                  retainedFileCountLimit: 7,
+                  outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
+    .Filter.ByExcluding(Matching.FromSource("Microsoft.EntityFrameworkCore"))
+    //.Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore"))
+    .Filter.ByExcluding(Matching.FromSource("Microsoft.Hosting"))
+    .CreateLogger();
+
+var parser = new FileIniDataParser();
+IniData data = parser.ReadFile(builder.Configuration.GetConnectionString("ConfigConnection"));
+string host = data["BD"]["host"];
+
+if (host != "localhost" && host != "Localhost")
+{
+    throw new InvalidOperationException("The server can only be started on localhost.");
+}
+
+var end = data["BD"]["end"];
+
+var firebirdConnectionString = $"Server=127.0.0.1;Database={end};{builder.Configuration.GetConnectionString("FirebirdConnection")}";
+
+var sqliteConnectionString = builder.Configuration.GetConnectionString("SqliteConnection");
+
+builder.Services.AddDbContext<AppDbContextFirebird>(options =>
+    options.UseFirebird(firebirdConnectionString));
+
+builder.Services.AddDbContext<AppDbContextSqlite>(options =>
+    options.UseSqlite(sqliteConnectionString));
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddSingleton(builder.Configuration);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    var kestrelConfig = builder.Configuration.GetSection("Kestrel");
+    serverOptions.Configure(kestrelConfig);
+});
+
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContextSqlite>();
+    dbContext.Database.EnsureCreated();
+
+    var areaService = scope.ServiceProvider.GetRequiredService<IAreaService>();
+    await areaService.SyncAreas();
+    var alphadigiService = scope.ServiceProvider.GetRequiredService<IAlphadigiService>();
+    await alphadigiService.SyncAlphadigi();
+    var condominioService = scope.ServiceProvider.GetRequiredService<CondominioService>();
+    await condominioService.SyncCondominio();
+
+    var schedulerService = new DailyTaskSchedulerService(() =>
+    {
+        alphadigiService.UpdateStage("DELETE").Wait();
+    });
+
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<RequestTimingMiddleware>();
+
+app.UseCors("AllowAllOrigins");
+
+app.MapCarter();
+
+app.Run();
